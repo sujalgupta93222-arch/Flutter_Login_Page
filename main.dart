63d0d4f93dad3ea8f8763f919bf39715e807fb0b
sujal gupta <sujalgupta93222@gmail.com>
2025-11-16 23:02:23 +0530

    Refactor login page and improve validation checks

--- main.dart
+++ main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title:'LoginPage',
       home: LoginPage(),
     );
   }
@@ -171,4 +172,5 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+
 
